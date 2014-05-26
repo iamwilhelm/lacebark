86d4ffa33cec260fbed9113339a87b4d9c49dcc3
Wil Chung <iamwil@gmail.com>
2014-05-26 10:11:32 -0700

    add toolbar

--- lacebark.elm
+++ lacebark.elm
@@ -8,6 +8,27 @@ cursor = filled darkCharcoal (rect 10 10)
 input = (,) <~ lift inSeconds (fps 30)
              ~ lift2 relativeMouse (center <~ Window.dimensions) Mouse.position
 
+controlScene (mouseX, mouseY) =
+  [ move (toFloat mouseX, toFloat mouseY)
+      cursor
+
+  , move (toFloat -350, toFloat 290)
+      (toForm (asText (mouseX, mouseY)))
+  ]
+
+toolbar =
+  flow down [
+    collage 80 80 (map (\x -> scale 0.25 x) myScene)
+  , image 80 80 "./yogi.jpg"
+  ]
+
+render (w, h) (dt, mouse) =
+  color lightGray <| container w h middle
+    <| flow right [
+         toolbar
+       , color gray
+           <| collage 1024 600 (myScene ++ (controlScene mouse))
+       ]
 
 areaRect w h = w * h
 
@@ -17,28 +38,17 @@ rectangle w h colr =
     toForm (asText (areaRect w h))
   ]
 
-render (w, h) (dt, mouse) = color lightGray
-                              <| container w h  middle
-                              <| color gray
-                              <| collage 1024 600 (myScene ++ (controlScene mouse))
-
-controlScene (mouseX, mouseY) =
-  [ move (toFloat mouseX, toFloat mouseY)
-      cursor
-
-  , move (toFloat -350, toFloat 290)
-      (toForm (asText (mouseX, mouseY)))
-  ]
 
 myScene =
-  [ outlined (dashed blue) (ngon 5 300)
-  , filled red (circle 75)
+  [ --outlined (dashed blue) (ngon 5 300)
+    filled red (circle 75)
   , rotate (degrees 60)
       (move (0, 0)
         (rectangle 100 100 yellow))
   , move (40, 40)
       (rectangle 80 80 red)
 
+  , (rectangle 20 20 green)
   --, toForm (asText "hello world")
   ]
 
