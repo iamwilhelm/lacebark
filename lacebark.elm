832db87deea2de36072fe0ecdace2f49c34016f2
Wil Chung <iamwil@gmail.com>
2014-05-31 22:18:00 -0700

    render using separate glyphForms functions and glyph records

--- lacebark.elm
+++ lacebark.elm
@@ -22,127 +22,155 @@ toolbarAdjust (x, y) = (x + 80, y)
 
 
 -- a glyph is a combination of shapes. glyph can be composed of many other shapes or itself 
-type Glyph = { pos: Vec, vel: Vec, rad: Float, colr:Color, formGroup:Form }
+type Glyph = { pos: Vec, vel: Vec, rad: Float, colr: Color, dim: Vec, radius: Float }
 
 initialGlyph = { pos = (0, 0)
                , vel = (0, 0)
                , rad = 0
+
                , colr = blue
-               , formGroup = group [ filled red <| ngon 5 40 ]
+               , dim = (10, 10)
+               , radius = 75
                }
 
-cursorArrowGlyph = { initialGlyph |
-    formGroup <- scale 0.4 <| group [
-                   filled black <| ngon 3 20
-                 , move (8.25, -15)
-                   <| rotate (degrees 30)
-                   <| filled black <| rect 10 25
-                 ] }
+cursorArrowGlyph = initialGlyph
 
-cursorHandGlyph = { initialGlyph |
-    formGroup <- scale 0.4 <| group [
-                   filled black <| circle 20
-                 ] }
+cursorArrowGlyphForm : Glyph -> Form
+cursorArrowGlyphForm glyph =
+  scale 0.4
+  <| group [
+       filled black <| ngon 3 20
+     , move (8.25, -15)
+       <| rotate (degrees 30)
+       <| filled black <| rect 10 25
+     ]
+
+cursorArrow = (cursorArrowGlyph, cursorArrowGlyphForm)
+
+
+cursorHandGlyph = initialGlyph
+
+cursorHandGlyphForm : Glyph -> Form
+cursorHandGlyphForm glyph =
+  scale 0.4
+  <| group [
+       filled black <| circle 20
+     ]
+
+cursorHand = (cursorHandGlyph, cursorHandGlyphForm)
+
 
 rectangleGlyph = { initialGlyph |
-    formGroup <- group [
-                   filled blue <| rect 120 120
-                 ] }
+    colr <- orange
+  , dim <- (120, 120) }
 
-circleGlyph = { initialGlyph |
-    formGroup <- group [
-                   filled yellow <| circle 75
-                 ] }
+rectangleGlyphForm: Glyph -> Form
+rectangleGlyphForm glyph =
+  group [
+    filled glyph.colr <| uncurry rect glyph.dim
+  ]
+
+rectangle = (rectangleGlyph, rectangleGlyphForm)
+
+--circleGlyph = { initialGlyph |
+--    formGroup <- group [
+--                   filled yellow <| circle 75
+--                 ] }
+--
+--
+--clubGlyph = { initialGlyph |
+--    formGroup <- group [
+--                   move (0, 60)
+--                     <| filled charcoal <| circle 40
+--                 , move (35, 0)
+--                     <| filled charcoal <| circle 40
+--                 , move (-35, 0)
+--                     <| filled charcoal <| circle 40
+--                 , move (0, -35)
+--                     <| filled charcoal <| rect 20 80
+--                 ] }
+--
+--heartGlyph = { initialGlyph |
+--    formGroup <- group [
+--                   move (30, 30)
+--                     <| filled red <| circle 50
+--
+--                 , move (-30, 30)
+--                     <| filled red <| circle 50
+--
+--                 , rotate (degrees 45)
+--                     <| move (0, 0)
+--                       <| filled red
+--                         <| rect 100 100
+--                 ] }
+--
+--diamondGlyph = { initialGlyph |
+--    formGroup <- group [ filled red <| ngon 4 80 ] }
+--
+--scratchGlyph = { initialGlyph |
+--    formGroup <- group [
+--                   include clubGlyph
+--                 ,
+--                   move (80, 80)
+--                     <| include heartGlyph
+--                 ] }
+--
+--include : Glyph -> Form
+--include glyph = move glyph.pos glyph.formGroup
 
 
-clubGlyph = { initialGlyph |
-    formGroup <- group [
-                   move (0, 60)
-                     <| filled charcoal <| circle 40
-                 , move (35, 0)
-                     <| filled charcoal <| circle 40
-                 , move (-35, 0)
-                     <| filled charcoal <| circle 40
-                 , move (0, -35)
-                     <| filled charcoal <| rect 20 80
-                 ] }
 
-heartGlyph = { initialGlyph |
-    formGroup <- group [
-                   move (30, 30)
-                     <| filled red <| circle 50
-
-                 , move (-30, 30)
-                     <| filled red <| circle 50
-
-                 , rotate (degrees 45)
-                     <| move (0, 0)
-                       <| filled red
-                         <| rect 100 100
-                 ] }
-
-diamondGlyph = { initialGlyph |
-    formGroup <- group [ filled red <| ngon 4 80 ] }
-
-scratchGlyph = { initialGlyph |
-    formGroup <- group [
-                   include rectangleGlyph
-                 ,
-                   move (80, 80)
-                     <| include circleGlyph
-                 ] }
-
-include : Glyph -> Form
-include glyph = move glyph.pos glyph.formGroup
-
-initialGlyphTools = [ scratchGlyph, rectangleGlyph, circleGlyph, clubGlyph, heartGlyph, diamondGlyph ]
-
-
-type Scene = { camera:Float, rootGlyph:Glyph, glyphTools:[Glyph], cursor:Glyph }
+type Scene = { camera: Float, glyphTools: [(Glyph, Glyph -> Form)], cursor: (Glyph, Glyph -> Form) }
 initialScene = { camera = 0
-               , glyphTools = initialGlyphTools
-               , rootGlyph = head initialGlyphTools
-               , cursor = cursorArrowGlyph
+               , glyphTools = [ rectangle ] --, scratchGlyph, circleGlyph, clubGlyph, heartGlyph, diamondGlyph ]
+               , cursor = cursorArrow
                }
 
 
-updateGlyph : Time -> (Int, Int) -> Glyph -> Glyph
-updateGlyph dt (mouseX, mouseY) g = { g | pos <- vecAdd g.pos <| vecMulS g.vel dt }
+updateGlyph : Time -> (Int, Int) -> (Glyph, Glyph -> Form) -> (Glyph, Glyph -> Form)
+updateGlyph dt (mouseX, mouseY) (glyph, glyphForm) = ({ glyph |
+    pos <- vecAdd glyph.pos <| vecMulS glyph.vel dt
+  },
+  glyphForm)
 
-updateGlyphTools : Time -> (Int, Int) -> [Glyph] -> [Glyph]
+updateGlyphTools : Time -> (Int, Int) -> [(Glyph, Glyph -> Form)] -> [(Glyph, Glyph -> Form)]
 updateGlyphTools dt (mouseX, mouseY) glyphTools =
   map (\glyph -> updateGlyph dt (mouseX, mouseY) glyph) glyphTools
 
-updateCursor : Time -> (Int, Int) -> Bool -> Glyph -> Glyph
-updateCursor dt (mouseX, mouseY) mouseDown glyph =
-  { glyph | pos <- (toFloat mouseX, toFloat mouseY) }
+updateCursor : Time -> (Int, Int) -> Bool -> (Glyph, Glyph -> Form) -> (Glyph, Glyph -> Form)
+updateCursor dt (mouseX, mouseY) mouseDown (glyph, glyphForm) =
+  ({ glyph | pos <- (toFloat mouseX, toFloat mouseY) }, glyphForm)
 
 updateScene : (Time, (Int, Int), Bool) -> Scene -> Scene
-updateScene (dt, mouse, mouseDown) s =
+updateScene (dt, mouse, mouseDown) scene =
   let cur = case mouseDown of
             True ->
-              cursorHandGlyph
+              cursorHand
             False ->
-              cursorArrowGlyph
-  in { s | rootGlyph <- updateGlyph dt mouse s.rootGlyph
-         , glyphTools <- updateGlyphTools dt mouse s.glyphTools
-         , cursor <- updateCursor dt mouse mouseDown cur }
+              cursorArrow
+  in { scene | glyphTools <- updateGlyphTools dt mouse scene.glyphTools
+             , cursor <- updateCursor dt mouse mouseDown cur }
 
+
+
+renderGlyph (glyph, glyphform) =
+  move glyph.pos <| glyphform glyph
+
+renderScene scene =
+  [ renderGlyph (head scene.glyphTools) ] -- , renderGlyph scene.cursor ]
+
+--renderToolbar { camera, glyphTools } =
+--  flow down
+--  <| map (\glyph -> collage 80 80 [scale 0.2 <| renderGlyph glyph !missing glyphForm! ]) glyphTools
 
 render : (Int, Int) -> Scene -> Element
 render (w, h) scene =
-  let renderGlyph {rad, colr, pos, formGroup} =
-        move pos <| formGroup
-      renderScene {camera, rootGlyph, glyphTools, cursor} =
-        [ renderGlyph rootGlyph, renderGlyph cursor ]
-      renderToolbar {camera, glyphTools} =
-        flow down <| map (\glyph -> collage 80 80 [scale 0.2 <| renderGlyph glyph]) glyphTools
-  in  color lightGray
-        <| container w h middle
-        <| flow right [
-             renderToolbar scene
-           , color gray <| collage 1024 600 <| renderScene scene
-           ]
+  color lightGray
+    <| container w h middle
+    <| flow right [
+         -- renderToolbar scene
+         color gray <| collage 1024 600 <| renderScene scene
+       ]
 
 
 clock = lift inSeconds (fps 30)
