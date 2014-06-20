3127c1683c4a4bf9c8677b08970f184e9d14c4bd
Wil Chung <iamwil@gmail.com>
2014-06-20 08:21:57 -0700

    restructure input structure and pipeline calls

--- gpipeline.elm
+++ gpipeline.elm
@@ -14,8 +14,6 @@ import Vec
 import Entity (..)
 import Glyph
 import Camera
-import Math.Matrix4
-import Math.Vector3
 
 glyph2WorldMat : Glyph.Glyph -> Transform2D.Transform2D
 glyph2WorldMat (entity, entityForm) =
@@ -49,33 +47,5 @@ renderInViewportFrame : [Form] -> [Form] -> [Form]
 renderInViewportFrame inFrameForms childFrameForms =
   -- TODO clip all objects outside the viewport
   groupTransform camera2ViewportMat childFrameForms :: inFrameForms
-
-mouseInCameraFrame : Camera.Viewport -> (Float, Float) -> (Float, Float)
-mouseInCameraFrame viewport mousePos =
-  let
-    center : (Float, Float) -> (Float, Float)
-    center (w, h) = (w / 2, h / 2)
-
-    offset = center viewport.dim
-
-    v = Math.Matrix4.transform (
-          Math.Matrix4.scale3 1 -1 1
-          <| Math.Matrix4.translate3 -(fst offset) (snd offset) 0
-          <| Math.Matrix4.makeRotate (degrees -viewport.rot) Math.Vector3.k
-        )
-        (Math.Vector3.vec3 (fst mousePos) (snd mousePos) 0)
-  in
-    (Math.Vector3.getX v, Math.Vector3.getY v)
-
-mouseInWorldFrame : Camera.Camera -> (Float, Float) -> (Float, Float)
-mouseInWorldFrame camera mousePos =
-  let
-    v = Math.Matrix4.transform (
-        Math.Matrix4.translate3 -(fst camera.pos) -(snd camera.pos) 0
-        <| Math.Matrix4.makeRotate (degrees -camera.rot) Math.Vector3.k
-      )
-      (Math.Vector3.vec3 (fst mousePos) (snd mousePos) 0)
-  in
-    (Math.Vector3.getX v, Math.Vector3.getY v)
 
 
--- main.elm
+++ main.elm
@@ -9,6 +9,14 @@ import Axes
 import Camera
 import Gpipeline
 
+import Math.Matrix4
+import Math.Vector3
+
+
+-- config
+
+windowDim = (1024, 600)
+
 -- TODO Scene and tools overlay and cursor overlay should all be inserted in
 -- different parts of the graphics transformation pipeline
 
@@ -59,30 +67,29 @@ addGlyph { dt, mousePos, mouseDown } (entity, prevEntityForm) =
   in
     (entity, newEntityForm)
 
+stampGlyph : AppInput -> Scene -> Scene
+stampGlyph appInput scene =
+  let
+    headSet = take 0 scene.glyphTools
+    tailSet = drop 0 scene.glyphTools
+    stepGlyphTools = headSet ++ (addGlyph appInput <| head tailSet) :: tail tailSet
+  in
+    { scene | glyphTools <- stepGlyphTools }
+
 updateGlyphTools : AppInput -> [Glyph.Glyph] -> [Glyph.Glyph]
 updateGlyphTools appinput glyphTools =
-  case appinput.mouseDown of
-    True ->
-      let
-        headSet = take 0 glyphTools
-        tailSet = drop 0 glyphTools
-      in
-        headSet ++ (
-            addGlyph appinput <| head tailSet
-          ) :: tail tailSet
-    False ->
-      map (\glyph ->
-        updateGlyph appinput glyph
-      ) glyphTools
-
+  map (\glyph ->
+    updateGlyph appinput glyph
+  ) glyphTools
 
 updateCursor : AppInput -> Glyph.Glyph -> Glyph.Glyph
 updateCursor { mousePos, mouseDown } _ =
   let (entity, entityForm) =
-    if mouseDown == True then
-      Glyph.closedPawCursor
-    else
-      Glyph.openPawCursor
+    case mouseDown of
+      True ->
+        Glyph.closedPawCursor
+      _ ->
+        Glyph.openPawCursor
   in
     ({ entity | pos <- mousePos }, entityForm)
 
@@ -100,30 +107,48 @@ updateCamera { dt, keyDir } camera =
 updateViewport : AppInput -> Camera.Viewport -> Camera.Viewport
 updateViewport { winDim } viewport =
   { viewport |
-      dim <- tupInt2Float winDim
+      dim <- floatify winDim
   }
 
-updateScene : AppInput -> Scene -> Scene
+--updateScene : Event -> Scene -> Scene
 updateScene appInput scene =
   let
-    stepViewport = updateViewport appInput scene.viewport
 
-    mouseInCamera = Gpipeline.mouseInCameraFrame stepViewport appInput.mousePos
-    appInputInCamera = { appInput - mousePos | mousePos = mouseInCamera }
+    stepCursor = updateCursor (inCamera scene.viewport <| appInput) scene.cursor
 
-    stepCursor = updateCursor appInputInCamera scene.cursor
+    stepCamera = updateCamera
+                   (inWorld scene.camera . inCamera scene.viewport <| appInput)
+                   scene.camera
 
-    mouseInWorld = Gpipeline.mouseInWorldFrame scene.camera appInputInCamera.mousePos
-    appInputInWorld = { appInput - mousePos | mousePos = mouseInWorld }
+    stepGlyphTools = updateGlyphTools
+                       (inWorld scene.camera . inCamera scene.viewport <| appInput)
+                       scene.glyphTools
 
-    stepCamera = updateCamera appInputInWorld scene.camera
-    stepGlyphTools = updateGlyphTools appInputInWorld scene.glyphTools
 
   in
     { scene | camera <- stepCamera
             , glyphTools <- stepGlyphTools
             , cursor <- stepCursor
-            , viewport <- stepViewport }
+    }
+
+updateApp : AppInput -> Scene -> Scene
+updateApp appInput scene =
+  -- when click
+  -- when trace (dragging while recording path)
+  -- when drag
+  -- when hover
+  -- when move mouse
+  let
+    stepScene = { scene | viewport <- updateViewport appInput scene.viewport }
+  in
+    case appInput.mouseDown of
+      True ->
+        updateScene appInput . stampGlyph
+          (inWorld stepScene.camera . inCamera stepScene.viewport <| appInput)
+          <| stepScene
+      False ->
+        updateScene appInput stepScene
+
 
 -- TODO convert all draw() methods to toForm methods in namespace
 renderScene : Scene -> [Form]
@@ -160,31 +185,71 @@ type AppInput = {
   }
 
 
-tupInt2Float (x, y) = (toFloat x, toFloat y)
+floatify (x, y) = (toFloat x, toFloat y)
 
 input : Signal AppInput
 input =
   let
-    relativeMouse (ox, oy) (x, y) = (x - ox, -(y - oy))
-    center (w, h) = (div w 2, div h 2)
-
     clockInput : Signal Time
     clockInput = lift inSeconds (fps 60)
 
     mouseInput : Signal (Float, Float)
-    mouseInput = sampleOn clockInput <| tupInt2Float <~ Mouse.position
+    mouseInput =
+      sampleOn clockInput
+      <| floatify <~ Mouse.position
 
     --mouseDownInput : Signal Bool
-    mouseDownInput = sampleOn clockInput Mouse.isDown
-
-    --windowInput : Signal (Int, Int)
-    windowInput = Window.dimensions
+    mouseDownInput =
+      sampleOn clockInput
+      <| dropRepeats Mouse.isDown
 
     --keyInput = Singal { x: Int, y: Int }
-    keyInput = Keyboard.wasd
+    keyInput =
+      sampleOn clockInput Keyboard.wasd
+
+    --windowInput : Signal (Int, Int)
+    windowInput =
+      sampleOn clockInput Window.dimensions
   in
     AppInput <~ clockInput ~ mouseInput ~ mouseDownInput ~ keyInput ~ windowInput
 
-windowDim = (1024, 600)
-main = render <~ Window.dimensions ~ foldp updateScene initialScene input
+mouseInCameraFrame : Camera.Viewport -> (Float, Float) -> (Float, Float)
+mouseInCameraFrame viewport mousePos =
+  let
+    center : (Float, Float) -> (Float, Float)
+    center (w, h) = (w / 2, h / 2)
+
+    offset = center viewport.dim
+
+    v = Math.Matrix4.transform (
+          Math.Matrix4.scale3 1 -1 1
+          <| Math.Matrix4.translate3 -(fst offset) (snd offset) 0
+          <| Math.Matrix4.makeRotate (degrees -viewport.rot) Math.Vector3.k
+        )
+        (Math.Vector3.vec3 (fst mousePos) (snd mousePos) 0)
+  in
+    (Math.Vector3.getX v, Math.Vector3.getY v)
+
+inCamera : Camera.Viewport -> AppInput -> AppInput
+inCamera viewport ({ mousePos } as appInput) =
+  { appInput - mousePos | mousePos = mouseInCameraFrame viewport mousePos }
+
+mouseInWorldFrame : Camera.Camera -> (Float, Float) -> (Float, Float)
+mouseInWorldFrame camera mousePos =
+  let
+    v = Math.Matrix4.transform (
+        Math.Matrix4.translate3 -(fst camera.pos) -(snd camera.pos) 0
+        <| Math.Matrix4.makeRotate (degrees -camera.rot) Math.Vector3.k
+      )
+      (Math.Vector3.vec3 (fst mousePos) (snd mousePos) 0)
+  in
+    (Math.Vector3.getX v, Math.Vector3.getY v)
+
+inWorld : Camera.Camera -> AppInput -> AppInput
+inWorld camera ({ mousePos } as appInput) =
+  { appInput - mousePos | mousePos = mouseInWorldFrame camera mousePos }
+
+
+
+main = render <~ Window.dimensions ~ foldp updateApp initialScene input
 
