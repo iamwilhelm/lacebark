8209de1370176aee00545aab134a891bae14eebb
Wil Chung <iamwil@gmail.com>
2014-08-31 16:25:02 -0700

    added vector functions

--- vec.elm
+++ vec.elm
@@ -5,6 +5,17 @@ type Vec = (Float, Float)
 add : Vec -> Vec -> Vec
 add (ax, ay) (bx, by) = (ax + bx, ay + by)
 
+sub : Vec -> Vec -> Vec
+sub (ax, ay) (bx, by) = (ax - bx, ay - by)
+
+dot : Vec -> Vec -> Float
+dot (ax, ay) (bx, by) = ax * bx + ay * by
+
+dist : Vec -> Float
+dist (x, y) = sqrt <| x * x + y * y
+
 mulS : Vec -> Float -> Vec
 mulS (x, y) a = (x * a, y * a)
 
+scale : Vec -> Vec -> Vec
+scale (x, y) (sx, sy) = (x * sx, y * sy)
